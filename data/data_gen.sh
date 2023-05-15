57cf3f76ae48eea36389ac792d2a4c39c97244cb
njsyw1997 <shaoyw15@gmail.com>
2023-05-14 22:43:46 -0400

    commit

---
+++ data/data_gen.sh
@@ -0,0 +1,4 @@
+for l in 2048 4096 8192 16384
+do
+    python data_gen.py  ${l} 1024 random
+done
--- submit.sh
+++ submit.sh
@@ -1,8 +1,10 @@
-for n in 1 2 4 8
+for l in 2048 4096 8192 16384 32768 
 do
-    for t in 1 2 4 8
+    for n in 1 2 4 8 16
     do
-cat <<EOF >n${n}_t${t}.sbatch
+        for t in 1
+        do
+cat <<EOF >n${n}_t${t}_l${l}.sbatch
 #!/bin/bash
 
 #SBATCH --job-name=attention
@@ -12,16 +14,17 @@ cat <<EOF >n${n}_t${t}.sbatch
 #SBATCH --mem=6GB 
 #SBATCH --time=01:00:00 
 #SBATCH --verbose
-#SBATCH --output=n${n}_t${t}.out
+#SBATCH --output=result/n${l}d1024/n${n}_t${t}.out
 
 module purge 
 module load gcc/10.2.0
 module load openmpi/gcc/4.0.5 
 export OMP_NUM_THREADS=\$SLURM_CPUS_PER_TASK
 export OMP_THREAD_LIMIT=\$SLURM_CPUS_PER_TASK
-mpiexec ./main n32768d1024
+mpiexec ./main n${l}d1024
 EOF
-    sbatch n${n}_t${t}.sbatch
-    rm n${n}_t${t}.sbatch
+    sbatch n${n}_t${t}_l${l}.sbatch
+    rm n${n}_t${t}_l${l}.sbatch
+        done
     done
 done
